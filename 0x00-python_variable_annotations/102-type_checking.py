cb536ceae81c2100c20bfdc9c84f750b7515e929
Shumani <shumanisimeli@gmail.com>
2024-07-04 23:09:11 +0200

    created 102-type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""102-type_checking.py"""
+from typing import Tuple, Iterable, List, Union
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Return a list"""
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
