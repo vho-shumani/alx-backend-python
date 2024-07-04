d1cb29bcee3ce7c11141027ea1b3cba014561351
Shumani <shumanisimeli@gmail.com>
2024-07-04 18:41:50 +0200

    created 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""7-to_kv.py"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """Return a tuple"""
+    return (k, v**2)
