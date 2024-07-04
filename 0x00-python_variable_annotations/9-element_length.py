379b8b1aa1d7c3480a600d729c496236e8bfab80
Shumani <shumanisimeli@gmail.com>
2024-07-04 21:56:06 +0200

    created 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""9-element_length.py"""
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns a list"""
+    return [(i, len(i)) for i in lst]
