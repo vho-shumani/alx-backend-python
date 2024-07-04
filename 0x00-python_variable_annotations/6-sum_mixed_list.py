10ff3f8478863711fed75797d55a20416a7085fb
Shumani <shumanisimeli@gmail.com>
2024-07-04 18:21:22 +0200

    created 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list.py"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum as a float"""
+    return float(sum(mxd_lst))
